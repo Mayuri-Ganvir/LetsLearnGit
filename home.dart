54feb66bdbc9fb9192a2183b40f86e62d84fab06
Mayuri Ganvir <mayuri.kishul@gmail.com>
2022-06-08 12:21:36 +0530

    adding home.dart

---
+++ home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // ignore: prefer_const_constructors
+  runApp(Material(
+      // ignore: prefer_const_constructors
+      color: Color.fromARGB(221, 30, 204, 204),
+      // ignore: prefer_const_constructors
+      child: Center(
+        // ignore: prefer_const_constructors
+        child: Text(
+          " home page",
+          textDirection: TextDirection.ltr,
+          // ignore: prefer_const_constructors
+          
+        ),
+      )));
+}
