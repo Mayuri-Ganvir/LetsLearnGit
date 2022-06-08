1905de01d6cbae608167fdd645c537c5d3aeb61d
Mayuri Ganvir <mayuri.kishul@gmail.com>
2022-06-08 14:47:04 +0530

    added multiply.dart

---
+++ multiply.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // ignore: prefer_const_constructors
+  runApp(Material(
+      // ignore: prefer_const_constructors
+      
+      // ignore: prefer_const_constructors
+      child: Center(
+        // ignore: prefer_const_constructors
+        child: Text(
+          "multiply",
+          textDirection: TextDirection.ltr,
+          // ignore: prefer_const_constructors
+          
+        ),
+      )));
+}
